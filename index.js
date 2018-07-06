c5a213df4cdfc746b82fe84b5794602196430e72
Josh Grossman <tghosth@users.noreply.github.com>
2018-07-06 10:48:45 +0300

    Initial hello world commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Simple hello world function.
+ *
+ * @param  {String} name
+ * @return {String}
+ */
+module.exports = {
+    HelloWorld: function(name) {
+      return `Hello ${name}`;
+    }
+  };
